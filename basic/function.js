279ae147da1e634f6e87428f3032cb74143e99c9
amit kumar <amitarya95080@gmail.com>
2025-05-20 11:13:54 +0530

    Manipulated of function with an object and an array

--- basic/function.js
+++ basic/function.js
@@ -1,5 +1,4 @@
 // create a function which is add two number
-
 function addTwoNumber(num1,num2){
     // console.log(num1+num2);
 }
@@ -23,6 +22,39 @@ function loginUserMessage(username){
     return `${username} Just logged in`
 }
 
-console.log(loginUserMessage());
-console.log(loginUserMessage("Aryan"));
+// console.log(loginUserMessage());
+// console.log(loginUserMessage("Aryan"));
 
+// function calPrice(...nums1){
+//     return nums1;
+// }
+
+function calPrice(n1,n2,...nums1){
+    return nums1;
+}
+
+// console.log(calPrice(300,200,500,300,600,500,100));
+
+
+// Create a function and passes the object as an argument;
+
+const user={
+    userName:"Aryan",
+    price:999
+}
+function handleObject(anyObject){
+    console.log(`UserName is ${anyObject.userName} and Price is ${anyObject.price}`)
+}
+
+handleObject(user)
+handleObject({userName:"A K Aryan",price:5999});
+
+
+// Similarly Create a function and passes the Array as an argument;
+const arr=[23,42,56,42,65,75];
+
+function getSecondValue(anyarr){
+    return arr[2];
+}
+console.log(getSecondValue(arr))
+console.log(getSecondValue([35,65,75]))
